338ea944b57cfaebbe9a6da7b5f745cc4abce02a
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-03-16 00:32:55 +0100

    add example

---
+++ example01.js
@@ -0,0 +1,16 @@
+require('.')({global: true});
+
+Np(N(10));
+
+Pp(P(N(4))(N(7)));
+
+Lp(   L( N(4),N(7) )   );
+
+
+Np(N(10));
+Np(Ninc(N(10)));
+
+Np(  Nmul(N(10))(N(25))  );
+
+Np(  Nadd(N(10))(N(25))  );
+
