79209f02849033324d7a4b5e44763a8024f86365
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-03-16 00:31:37 +0100

    update index

--- index.js
+++ index.js
@@ -1,38 +1,76 @@
 
 "use strict";
 
-global.t = (a, b) => a;
-global.f = (a, b) => b;
-global.e = null;
+const T = a => b => a;
+const F = a => b => b;
+const N = n => n ? a => b => a(N(n-1)(a)(b)) : a => b => b;
+const Nv = a => a(x => x+1)(0);
+const P = a => b => f => f(a)(b);
+const H = a => a(T);
+const R = a => a(F);
+const La = a => a.length ? P(a[0])(La(a.slice(1))) : null;
+const L = function () { return La(Array.prototype.slice.call(arguments)); };
+const Np = a => console.log(Nv(a));
+const Pp = a => console.log(Nv(H(a)) + " " + Nv(R(a)));
+const Ls = a => a ? Nv(H(a)) + " " + Ls(R(a)) : "";
+const Lp = a => console.log(Ls(a));
 
-global.n0 = (f,x) => x;
-global.n1 = (f,x) => f(x);
-global.n2 = (f,x) => f(f(x));
+const Ninc = a => b => c => b(a(b)(c));
 
-global.n = (i) => (
-    i == 0 ? ((f,x) => x) : ( (f,x) => n(i-1)(f, x) )
-);
+const Nadd = a => b => c => d =>  a(c)(b(c)(d));
+const Nmul = a => b => c => d =>  a(b(c))(d);
 
-
-global.pair = (a, b) => ( (f) => f(a, b) );
-
-global.car = (p) => p(t);
-global.cdr = (p) => p(f);
-
-global.list = (...e) => {
-    var v, l = e;
-    while (e.length) {
-        v = e.pop();
-        l = pair(v, l);
+module.exports = function (opt) {
+    const e = {
+        T:T,
+        F:F,
+        N:N,
+        Nv:Nv,
+        P:P,
+        Pp:Pp,
+        H:H,
+        R:R,
+        La:La,
+        L:L,
+        Ls:Ls,
+        Lp:Lp,
+        Np:Np,
+        Ninc:Ninc,
+        Nadd:Nadd,
+        Nmul:Nmul
+    };
+    if (opt && opt.global) {
+        for (var k in e) {
+            if (e.hasOwnProperty(k)) {
+                global[k] = e[k];
+            }
+        }
     }
-    return l;
-};
+    return e;
+}
 
-global.inc = (x) => (x+1);
+// global.n0 = (f,x) => x;
+// global.n1 = (f,x) => f(x);
+// global.n2 = (f,x) => f(f(x));
+// global.n3 = (f,x) => f(n2(x));
 
-global.printPair = (p) => { console.log(car(p), cdr(p)); };
+// P(a)(b) - makes a pair of a and b
 
-// global.printList = (l) => { console.log(car(p), cdr(p)); };
 
-global.printNum = (x) => { console.log( x(inc,0) ); }
+// global.list = (...e) => {
+//     var v, l = e;
+//     while (e.length) {
+//         v = e.pop();
+//         l = pair(v, l);
+//     }
+//     return l;
+// };
+
+// global.inc = (x) => (x+1);
+
+// global.printPair = (p) => { console.log(car(p), cdr(p)); };
+
+// // global.printList = (l) => { console.log(car(p), cdr(p)); };
+
+// global.printNum = (x) => { console.log( x(inc,0) ); }
 
